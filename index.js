f58ac4a4efe419c794a330b3f244460394808361
INFO\32658368860 <wagnwnki@gmail.com>
2018-01-16 21:19:29 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var five = require("johnny-five");
+var board = new five.Board({port: "COM3"});
+
+board.on("ready", function() {
+ 
+    var button = new five.Button(2);
+    var led = new five.Led(9);
+    var servo = new five.Servo({    
+        pin: 10, 
+        startAt: 10
+    });
+    var portaoAberto = false;
+
+    button.on ("down",function(){
+        console.log("down");
+        if (portaoAberto){
+            servo.to(19,1*1000);
+            portaoAberto = false;
+            console.log("Portao fechado");
+            led.off();
+        }
+        else{
+            servo.to(90,1*1000);
+            portaoAberto = true;
+            console.log("Portao aberto");
+            led.on();
+        }
+    });
+
+    button.on ("hold",function(){
+        console.log("hold");
+    });
+
+    button.on ("up",function(){
+        console.log("up");
+    });
+
+});
