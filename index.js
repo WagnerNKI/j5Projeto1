cd9125a579202cdda9088d7124d6cec6168d6839
INFO\32658368860 <wagnwnki@gmail.com>
2018-01-18 20:46:22 -0200

    projeto portao final

--- index.js
+++ index.js
@@ -1,38 +1,98 @@
 var five = require("johnny-five");
-var board = new five.Board({port: "COM3"});
+var mqtt = require("mqtt");
+var board = new five.Board({ port: "COM3" });
 
-board.on("ready", function() {
- 
-    var button = new five.Button(2);
+
+
+
+board.on("ready", function () {
+    console.log("ready");
+
+    var buttonPortao = new five.Button(2);
+    var buttonLed = new five.Button(3);
     var led = new five.Led(9);
-    var servo = new five.Servo({    
-        pin: 10, 
-        startAt: 10 
+    var servo = new five.Servo({
+        pin: 10,
+        startAt: 10
     });
     var portaoAberto = false;
+    var ledOn = false;
 
-    button.on ("down",function(){
-        console.log("down");
-        if (portaoAberto){
-            servo.to(19,1*1000);
+    function acendeApagaLed() {
+        if (ledOn) {
+            led.off();
+            console.log("lampada apagada");
+            ledOn = false;
+        }
+        else {
+            led.on();
+            console.log("lampada acesa");
+            ledOn = true;
+        }
+    }
+
+    function abrefechaportao() {
+
+        if (portaoAberto) {
+            servo.to(19, 1 * 1000);
             portaoAberto = false;
             console.log("Portao fechado");
-            led.off();
         }
-        else{
-            servo.to(90,1*1000);
+
+        else {
+            servo.to(109, 1 * 1000);
             portaoAberto = true;
             console.log("Portao aberto");
-            led.on();
+        }
+    }
+
+
+
+    buttonPortao.on("down", function () {
+        console.log("down");
+        if (!ledOn && !portaoAberto){
+            abrefechaportao();
+            acendeApagaLed();        
+        }     
+        else{
+            abrefechaportao();
         }
     });
+    buttonLed.on("down", function () {
+        acendeApagaLed();
+    })
 
-    button.on ("hold",function(){
-        console.log("hold");
+    var client = mqtt.connect("mqtt://localhost");
+
+    client.on("connect", function () {
+
+        console.log("conectado");
+        client.subscribe("teste");
+
+        function callback(topic, paylaod) {
+
+            var msg = paylaod.toString();
+
+            if (msg === "portao") {
+                if (!ledOn && !portaoAberto){
+                    abrefechaportao();
+                    acendeApagaLed();        
+                }     
+                else{
+                    abrefechaportao();
+                }
+            }
+            else if (msg === "led") {
+                acendeApagaLed();
+            }
+            else {
+                console.log(topic);
+                console.log(msg);
+            }
+        }
+       
+        client.on("message", callback);
     });
 
-    button.on ("up",function(){
-        console.log("up");
-    });
+})
 
-});
